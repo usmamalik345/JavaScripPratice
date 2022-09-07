43b28f80e41b3097862569afb0ac7cee1b75d68b
Osama Malik <usmamalik344@gmail.com>
2022-09-07 12:26:19 +0500

    javscript pratice

---
+++ style.js
@@ -0,0 +1,10 @@
+food = Number( prompt("how much the price of food ")) 
+tipOfFood = Number(prompt("how much you want to give ")) / 100
+
+tipAmounnt = food * tipOfFood
+console.log(tipAmounnt , "this is tip");
+
+totalAmount = food + tipAmounnt
+
+    console.log(totalAmount);
+alert(totalAmount)
