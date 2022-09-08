4146ed779b7b2a06ec2a4b8382ffb1d9d94ff661
Osama Malik <usmamalik344@gmail.com>
2022-09-08 16:35:36 +0500

    pratice 2

---
+++ Pratice2.js
@@ -0,0 +1,10 @@
+function myfucntion() {
+    console.log("hello janu");
+}
+myfucntion()
+
+function myfucntion2(name){
+    console.log(name);
+}
+
+myfucntion2("hello world")
--- style.js
+++ style.js
@@ -1,10 +1,21 @@
-food = Number( prompt("how much the price of food ")) 
-tipOfFood = Number(prompt("how much you want to give ")) / 100
+// Prompt Concept
 
-tipAmounnt = food * tipOfFood
-console.log(tipAmounnt , "this is tip");
+// food = Number( prompt("how much the price of food ")) 
+// tipOfFood = Number(prompt("how much you want to give ")) / 100
 
-totalAmount = food + tipAmounnt
+// tipAmounnt = food * tipOfFood
+// console.log(tipAmounnt , "this is tip");
 
-    console.log(totalAmount);
-alert(totalAmount)
+// totalAmount = food + tipAmounnt
+
+//     console.log(totalAmount);
+// alert(totalAmount)
+
+
+// let weather = prompt("how is the weather")
+
+// if (weather == "rain"){
+//     console.log("Bring your Umberla ");
+// } else{
+//     console.log("wear Glasses");
+// }
